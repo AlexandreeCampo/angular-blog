1774eb71bcf0bfb0187e0d32110ec6482279e14e
Alexandre <alexandree.campo@gmail.com>
2023-02-12 11:00:44 -0300

    content created

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { __values } from 'tslib';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -8,9 +8,11 @@ import { __values } from 'tslib';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover: string = "https://lumiere-a.akamaihd.net/v1/images/werewolf_by_night_993ad125.jpeg?region=0,0,2250,2813"
-  contentTitle: string = "notice"
-  contentDescription: string ="description"
+  
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string =""
+  private id:string | null = "0"
 
   constructor(
     private route:ActivatedRoute
@@ -18,8 +20,17 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
   }
 
-}
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+      this.contentTitle = result.title
+      this.contentDescription = result.description
+      this.photoCover = result.photoCover
+    }
+  }
